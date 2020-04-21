3a020b5a232a83e1e8b4c499226c2233dbba5c05
alice737 <uzar.alicja@gmail.com>
2020-04-21 19:31:58 +0200

    simple add rule

--- active_firewall.py
+++ active_firewall.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python3
 
 import sys
+import os
+import re
 
 print("Firewall is active")
 
 attack_alerts = ["LAND ATTACK ALERT", "TCP PORT SCAN ALERT", "UDP FLOOD ALERT"]
+IP_REGEX = r"(\d{1,3}\.){3}\d{1,3}"
 
-def addRule(allert):
+def getRule(ip, port, protocol):
+	rule = "INPUT " + " -s " + str(ip) + " -j" + " DROP"
+	return '' if rule is None else str(rule)
+
+def addRule(allert, sourceIp):
 	print('Adding rule: ' + allert)
+	rule = getRule(sourceIp,'', '')
+	print('Rule'+ rule)
+	os.system(f"/sbin/iptables -A {rule}")
+
+def findIpAddress(line):
+	matched = re.search(IP_REGEX + " -> " + IP_REGEX, line);
+	print(matched)
+	if matched is not None:
+		print('Matched')
+		return matched.group().split(" -> ")[0]
 
 def activeFirewall(line):
 
 	for allert in attack_alerts:
 		if allert in line:
 			print('Find attack: ' +  allert)
-			addRule(allert)
-
+			sourceIp = findIpAddress(line)
+			addRule(allert, sourceIp)
 
 for line in sys.stdin:
 	activeFirewall(line)
