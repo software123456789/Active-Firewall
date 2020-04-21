ca11cc3e27409207fb1921ceb4f35f4ef601c381
alice737 <uzar.alicja@gmail.com>
2020-04-21 17:35:10 +0200

    init

--- active_firewall.py
+++ active_firewall.py
@@ -4,5 +4,18 @@ import sys
 
 print("Firewall is active")
 
+attack_alerts = ["LAND ATTACK ALERT", "TCP PORT SCAN ALERT", "UDP FLOOD ALERT"]
+
+def addRule(allert):
+	print('Adding rule: ' + allert)
+
+def activeFirewall(line):
+
+	for allert in attack_alerts:
+		if allert in line:
+			print('Find attack: ' +  allert)
+			addRule(allert)
+
+
 for line in sys.stdin:
-	print('Python: ' + line)
+	activeFirewall(line)
